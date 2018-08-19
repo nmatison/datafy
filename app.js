05ebd1a45ad1bcd4254ffeb6cb487af4e58adfaf
Nick <nmatison@gmail.com>
2018-08-19 11:57:42 -0700

    data

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-d3.csv('regional-global-weekly-2017-06-30--2017-07-07.csv', function(error,data){
-			stockData = data.slice(0, 381).filter(function(d){
-				if(d["Country"] == "United States"){
-					return true;
-				}
-				return false;
+d3.csv('./regional-global-weekly-2017-06-30--2017-07-07.csv', function(error,data){
+  if (error) {
+    console.log(error)
+  } else {
+    console.log(data);
+    dataset = data;
+  }
+
 			})
