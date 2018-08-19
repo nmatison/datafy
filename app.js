999407ca46d3b6de819663fdd06f5408e24d8b00
Nick <nmatison@gmail.com>
2018-08-19 12:08:11 -0700

    remove app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-d3.csv('./regional-global-weekly-2017-06-30--2017-07-07.csv', function(error,data){
-  if (error) {
-    console.log(error)
-  } else {
-    console.log(data);
-    dataset = data;
-  }
-});
