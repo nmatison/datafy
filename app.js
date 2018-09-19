f27f3b3f2c9ca8bef299d8fb6bf1305a05249d6d
Nick <nmatison@gmail.com>
2018-09-19 13:55:33 -0700

    Add util to front end to fetch token from back-end

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
 const express = require('express')
+const router = express.Router();
 const app = express()
 const path = require('path')
 const fetch = require('node-fetch')
 const PORT = process.env.PORT || 8000; // process.env accesses heroku's environment variables
-
-app.use(express.static('public'))
-
-app.get('/', (request, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'))
-})
+const formurlencoded = require("form-urlencoded").default;
 
 // create route to get single book by its isbn
 app.get('/token', (request, response) => {
   // make api call using fetch
+  let data = formurlencoded({
+    "grant_type": "client_credentials"
+  })
   fetch(`https://accounts.spotify.com/api/token`, {
     method: "POST",
-    mode: "cors",
+    mode: "no-cors",
     headers: {
-      "Content-Type": "application / x-www-form-urlencoded",
+      "Content-Type": "application/x-www-form-urlencoded",
       "Authorization": "Basic OWJiZmU1NDY2YmNmNDBmMWFjZTA5ZDE1NjQyODIyNGM6ZjhlYzBiNTZjMjQwNDc5NDk4ZjBjMzQ0NjA1MTc3MGE="
-    }
+    },
+    body: data
   })
-  .then((response) => {
-      return response.access_token;
-  }).then((body) => {
-      let results = JSON.parse(body)
-      console.log(results)   // logs to server
-      response.send(results) // sends to frontend
-    });
-});
-
-
-app.listen(PORT, () => {
+  .then((res) => {
+    return res.text()
+  })
+  .then((body) => {
+        let results = JSON.parse(body)
+        let token = results.access_token
+        console.log(token)   // logs to server
+        response.send(token) // sends to frontend
+      });
+  });
+  
+  app.use(express.static('public'))
+  
+  app.get('/', (request, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+  })
+  
+  app.listen(PORT, () => {
   console.log(__dirname);
   console.log(`listening on ${PORT}`)
 })
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,11 @@
-const axios = require('axios');
+import axios from 'axios';
 
 document.addEventListener('DOMContentLoaded', () => {
 
     axios.get(`/token`)
     .then((response) => {
-        var token = response; 
+        var token = response.data; 
+        console.log(token)
     })
     .catch(function (error) {
         console.log(error);
