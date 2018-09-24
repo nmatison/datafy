592b472ccdba0baee60e2d9474945cfdc816bb95
Nick <nmatison@gmail.com>
2018-09-24 16:16:57 -0700

    Update package json for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,6 @@ const config = {
     filename: 'bundle.js',
     path: path.join(__dirname, './public')
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  },
   plugins: [
     new webpack.ProgressPlugin()
   ],
